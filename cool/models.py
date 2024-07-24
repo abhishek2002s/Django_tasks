05794bc04ae88e8a67267af2a45d0769ce27dfe9
abhishek2002s <linkedin23356abhi@gmail.com>
2024-07-24 22:12:55 +0530

    task3

--- cool/models.py
+++ cool/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=55)
+    roll = models.IntegerField()
+    email = models.EmailField(max_length=255)
+
+class Address(models.Model):
+    house_no = models.IntegerField()
+    pincode = models.IntegerField()
+    city = models.CharField(max_length=50)
+
+class Detail(models.Model):
+    username = models.CharField(max_length=50)
+    dob = models.IntegerField()
+    age = models.IntegerField()
